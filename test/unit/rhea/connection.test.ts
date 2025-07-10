b7c9869bc797f3e15f40d89317ce8be2161ce92d
Andrea Pietroni <apietroni@coders51.com>
2025-07-10 10:35:50 +0200

    tests

--- test/unit/rhea/connection.test.ts
+++ test/unit/rhea/connection.test.ts
@@ -2,7 +2,6 @@ import { afterEach, beforeEach, describe, expect, test } from "vitest"
 import { host, port, username, password, numberOfConnections, eventually } from "../../support/util.js"
 import { Connection, Container, create_container } from "rhea"
 import { closeConnection, openConnection, openManagement } from "../../support/rhea_utils.js"
-import { isReconnectionParamsDetails } from "../../../src/connection.js"
 
 describe("Rhea connections", () => {
   let container: Container
@@ -40,18 +39,5 @@ describe("Rhea connections", () => {
     await eventually(async () => {
       await openManagement(connection)
     }, 4000)
-  })
-
-  test("is reconnection param detail", () => {
-    expect(isReconnectionParamsDetails(true)).to.eql(false)
-    expect(isReconnectionParamsDetails(false)).to.eql(false)
-    expect(isReconnectionParamsDetails(123)).to.eql(false)
-    expect(
-      isReconnectionParamsDetails({
-        initialReconnectDelay: 1,
-        maxReconnectDelay: 1,
-        reconnectLimit: 1,
-      })
-    ).to.eql(true)
   })
 })
