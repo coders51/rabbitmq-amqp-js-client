3a11d1deeaa297615c1b37831b0e5d941af2f1ee
magne <magnello@coders51.com>
2025-07-30 14:32:53 +0200

    wip: revert changes

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,78 @@
+import typescript from "@rollup/plugin-typescript"
 import nodePolyfills from "rollup-plugin-polyfill-node"
 import { nodeResolve } from "@rollup/plugin-node-resolve"
 import commonjs from "@rollup/plugin-commonjs"
 
 export default [
   {
-    input: "dist/index.js",
+    input: "src/index.ts",
     output: {
       format: "cjs",
       file: "dist/cjs/index.js",
       sourcemap: true,
     },
-    external: ["rhea", "uuid"],
-    plugins: [commonjs(), nodePolyfills()],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      typescript({
+        module: "Node16",
+        moduleResolution: "Node16",
+        tsconfig: "./tsconfig.build.json",
+        compilerOptions: {
+          outDir: "dist/cjs",
+          declaration: true,
+          declarationDir: "dist/cjs",
+        },
+      }),
+      nodePolyfills(),
+    ],
   },
   {
-    input: "dist/index.js",
+    input: "src/index.ts",
     output: {
       format: "es",
       file: "dist/esm/index.js",
       name: "RabbitmqClient",
       sourcemap: true,
     },
-    plugins: [commonjs(), nodeResolve(), nodePolyfills()],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      typescript({
+        module: "Node16",
+        moduleResolution: "Node16",
+        tsconfig: "./tsconfig.build.json",
+        compilerOptions: {
+          outDir: "dist/esm",
+          declaration: true,
+          declarationDir: "dist/esm",
+        },
+      }),
+      nodePolyfills(),
+    ],
   },
   {
-    input: "dist/index.js",
+    input: "src/index.ts",
     output: {
       format: "umd",
       file: "dist/umd/index.js",
       name: "RabbitmqClient",
       sourcemap: true,
     },
-    plugins: [commonjs(), nodeResolve(), nodePolyfills()],
+    plugins: [
+      commonjs(),
+      nodeResolve(),
+      typescript({
+        module: "Node16",
+        moduleResolution: "Node16",
+        tsconfig: "./tsconfig.build.json",
+        compilerOptions: {
+          outDir: "dist/umd",
+          declaration: true,
+          declarationDir: "dist/umd",
+        },
+      }),
+      nodePolyfills(),
+    ],
   },
 ]
