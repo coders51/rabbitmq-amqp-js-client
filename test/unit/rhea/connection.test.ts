4a672a02251c0da9ecaeabb3ec70c7ffd80cc5c2
magne <magnello@coders51.com>
2025-07-30 17:13:00 +0200

    wip: oauth2 setup

--- test/unit/rhea/connection.test.ts
+++ test/unit/rhea/connection.test.ts
@@ -1,5 +1,14 @@
 import { afterEach, beforeEach, describe, expect, test } from "vitest"
-import { host, port, username, password, numberOfConnections, eventually } from "../../support/util.js"
+import {
+  host,
+  port,
+  username,
+  password,
+  numberOfConnections,
+  eventually,
+  Token,
+  generateToken,
+} from "../../support/util.js"
 import { Connection, Container, create_container } from "rhea"
 import { closeConnection, openConnection, openManagement, openWebSocketConnection } from "../../support/rhea_utils.js"
 
@@ -21,6 +30,22 @@ describe("Rhea connections", () => {
       port,
       username,
       password,
+    })
+
+    await eventually(async () => {
+      expect(await numberOfConnections()).to.eql(1)
+    })
+  })
+
+  test("create an oauth2 connection", async () => {
+    const token = generateToken(60)
+    container = create_container()
+    // container.sasl.client_mechanisms().enable_xoauth2(username, "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGH")
+    console.log(container.sasl.Client.mechanism)
+    connection = await openConnection(container, {
+      host,
+      port,
+      username: "guest",
     })
 
     await eventually(async () => {
