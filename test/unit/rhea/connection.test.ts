da4234236ddcc9b173d6521a9259b4aaf9ffa362
magne <magnello@coders51.com>
2025-07-31 11:26:16 +0200

    chore: add oath2 test

--- test/unit/rhea/connection.test.ts
+++ test/unit/rhea/connection.test.ts
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, test } from "vitest"
-import { host, port, username, password, numberOfConnections, eventually } from "../../support/util.js"
+import { host, port, username, password, numberOfConnections, eventually, generateToken } from "../../support/util.js"
 import { Connection, Container, create_container } from "rhea"
 import { closeConnection, openConnection, openManagement, openWebSocketConnection } from "../../support/rhea_utils.js"
 
@@ -21,6 +21,21 @@ describe("Rhea connections", () => {
       port,
       username,
       password,
+    })
+
+    await eventually(async () => {
+      expect(await numberOfConnections()).to.eql(1)
+    })
+  })
+
+  test("create a connection using oauth2", async () => {
+    const token = generateToken(username, 60)
+
+    connection = await openConnection(container, {
+      host,
+      port,
+      username,
+      password: token,
     })
 
     await eventually(async () => {
