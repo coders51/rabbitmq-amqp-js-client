4e1bde67f9cce6eb3014bd5bbe325ef9a06bd71c
Andrea Pietroni <apietroni@coders51.com>
2025-09-10 16:17:52 +0200

    added tls params + test

---
+++ test/e2e/tls_connection.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from "vitest"
+import { createEnvironment, Environment } from "../../src/environment.js"
+import {
+  host,
+  username,
+  eventually,
+  numberOfConnections,
+  password,
+} from "../support/util.js"
+import { Connection } from "../../src/connection.js"
+import { readFile } from "fs/promises"
+
+describe("TLS Connection", () => {
+  let environment: Environment
+  let connection: Connection
+
+  test("creating a TLS connection", async () => {
+    const tls = {
+      ca: await readFile("./tls-gen/basic/result/ca_certificate.pem", "utf8"),
+      cert: await readFile(`./tls-gen/basic/result/client_rabbitmq_certificate.pem`, "utf8"),
+      key: await readFile(`./tls-gen/basic/result/client_rabbitmq_key.pem`, "utf8"),
+      rejectUnauthorized: true
+    }
+
+    environment = createEnvironment({ host, port: 5671, username, password, tls })
+
+    connection = await environment.createConnection()
+
+    await eventually(async () => {
+      expect(await numberOfConnections()).to.eql(1)
+    })
+    await connection.close()
+    await environment.close()
+  })
+})
